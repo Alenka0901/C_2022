ca479f7dd1283e8cb02785fa70d9cb63af43f222
Alenka0901 <100983503+Alenka0901@users.noreply.github.com>
2022-03-14 14:02:27 +0800

    Create lecture-1-2.cpp

---
+++ lecture-1-2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+int s=50;
+printf("Число %d\n ", s);
+float n=0.82;
+printf("Дробь %f\n ", n);
+char m='m';
+printf("Символ %d\n ", m);
+double z=-963.9;
+printf("Отрицательное %f\n ", z);
+
+int a=s/50;
+printf("%d\n ", a);
+double f=z*3;
+printf("%f\n ", f);
+float g=n+4;
+printf("%f\n ", g);
+int h=s%3;
+printf("%d\n ", h);
+return 0;
+}
