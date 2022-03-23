c8730185ff03066bda8ce0af720f115be265826d
Alenka0901 <100983503+Alenka0901@users.noreply.github.com>
2022-03-23 16:25:59 +0800

    Update lecture3-1.cpp
    Исправленный вариант.

--- lecture3-1.cpp
+++ lecture3-1.cpp
@@ -1,26 +1,19 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main()
-{ 
-    int A, B, s;
-    
-    cout << "Введите число A";
-    cin >> A;
-     cout << "Введите число B";
-     while (1){
-     cin >> B;
-     if (B<=A){
-         cout << "Введите число B больше A";
-         continue;
-     }
-     
-         for (int i = A; i!=B; i++){
-             s=i*3;
-             cout <<s<< "\n";
-         }
-         break;
-     }
-    
+{
+    int A,B;
+    int f=0;
+    printf("Введите число A: ");
+    scanf ("%d" , &A);
+    printf("Введите число B: ");
+    scanf ("%d" , &B);
+    FILE *output;
+    output = fopen ("output","w");
+    for (int i=A; i<=B; i++){
+        f=i*3;
+        fprintf (output, "%d\n",f);
+    }
+    fclose(output);
     return 0;
 }
